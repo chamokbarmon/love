d45e7f8578e394809f53072a7509b87dc80c274c
chamokBarmon <chamokbarmon123@gmail.com>
2023-05-13 23:26:46 +0600

    first commit

---
+++ love.js
@@ -0,0 +1,6 @@
+document.getElementById('love').addEventListener('click',function(){
+   document.getElementById('msglove').innerText='Yes, I love you sona 💟 '   
+})
+document.getElementById('loveyes').addEventListener('click',function(){
+   document.getElementById('msglove').innerText='Yes, I love you Jan amr 💝  '   
+})
